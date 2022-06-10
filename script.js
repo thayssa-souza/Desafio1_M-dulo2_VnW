2c221fff08f9f9f9ae7e70731ea9d9b1444f7a06
thayssa-souza <thayssa.souzaf@gmail.com>
2022-06-10 01:34:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Desafio 1 - Módulo 2")
+
+let name = "Thayssa"
+let number = 5
+let anoAtual = 2022
+let resultadoSoma = anoAtual + number
+console.log(resultadoSoma)
+
+let frase = "Estou mexendo no console.log"
+console.log(frase)
+
+let quartaFeira = false
+console.log(typeof quartaFeira)
